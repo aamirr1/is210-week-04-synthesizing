e102290b621140f02ef91bb95ebdf535cacf444d
Amir Abdullaev <abdullaev.amir@gmail.com>
2016-09-28 06:18:20 +0000

    Taska 1-3 Corrected

--- task_01.py
+++ task_01.py
@@ -1,30 +1,68 @@
-#!/usr/bin/env python
+#!usr/bin/env python
 # -*- coding: utf-8 -*-
-"""This is adocstring for assignment 4 task 1"""
+"""This is a docstring for assignment 4 Synthesizing task"""
 
-import datetime
 
-CURDATE = None
+from decimal import Decimal 
 
-def get_current_date():
-    """Created this function to display today's date.
+ABSOLUTE_DIFFERENCE = Decimal(273.15)
+
+
+def fahrenheit_to_celsius(degrees):
+    """function will return celsius.
 
     Args:
-        None
+        degrees(mixed): fahrenheit will be converted to celsi
         
-    Return:
-        date: current system time will be returned.
-     
-    Example:
-        >>> print task_01.CURDATE
-        None
-        >>> task_01.get_current_date()
-        datetime.date(2016, 9, 25)
-    
+    Returns:
+        decimal: Returns value in Celsius
+        
+    Examples:
+        >>> fahrenheit_to_celsius(212)
+        Decimal ('100')
     """
-    
-    return datetime.date.today()
 
-if __name__ == '__main__':
-    CURDATE = get_current_date()
-    print CURDATE
+    degrees = Decimal(degrees)
+    return (degrees - 32) * 5 /9
+
+
+def celsius_to_kelvin(degrees):
+    """This function will convert Celsius  to Kelvin.
+
+    Args:
+        degrees(mixed): celsius will be converted to kelvin
+        
+    Returns:
+        decimal: Returns value in Kelvin
+        
+    Examples:
+        >>> celsius_to_kelvin(100)
+        Decimal('373.1499999999999772626324557')
+    """
+
+    degrees = Decimal(degrees)
+    return degrees + ABSOLUTE_DIFFERENCE
+
+
+def fahrenheit_to_kelvin(degrees):
+    """This function will convert Fahrenheit to Kelvin.
+
+    Args:
+        degrees(mixed): fahrenheit will be converted to kelvin
+        
+    Returns:
+        decimal: Returns value in Kelvin
+        
+    Examples:
+        >>> fahrenheit_to_kelvin(212)
+        Decimal('373.1499999999999772626324557')
+    """
+
+    exm1 = fahrenheit_to_celsius(degrees)
+    exm2 = celsius_to_kelvin(exm1)
+    return exm2
+
+
+
+    
+
